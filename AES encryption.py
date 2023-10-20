34e72b338afd24a0c0e0ad3edcc0c5e78906a5ee
janak2002 <117075383+janak2002@users.noreply.github.com>
2023-10-20 10:11:01 +0200

    date: 20.10.2023

---
+++ AES encryption.py
@@ -0,0 +1,22 @@
+from Crypto.Random import get_random_bytes
+from Crypto.Protocol.KDF import PBKDF2
+
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+
+
+
+
+salt = b'\xa6\n\x14\xc8l\x124\xb2\xa6\xcd\xf1\x7f\x95 \xe9d\x7fN1H\\\xd8\r\xad\x1e\xfd\xbc\xaf\x85\xcf\xc28'
+password='mydoglili'
+
+key=PBKDF2(password,salt,32)
+message = b"I'm having so much fun"
+
+cipher = AES.new(key,AES.MODE_CBC)
+ciphered_data=cipher.encrypt(pad(message,AES.block_size))
+
+with open ('encrypted.bin','wb') as f:
+    f.write(cipher.iv)
+    f.write(ciphered_data)
